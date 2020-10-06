b59213c86c2f47ae29231e29ae15ea837a37f63a
Prasanga <101890972@student.swin.edu.au>
2020-10-06 20:35:25 +1100

    add react router to implement spa functionality

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 import Rating from './Rating'
 
 const Product = ({product: {_id, image, name, rating, numReviews, price}}) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${_id}`}>
+            <Link to={`/product/${_id}`}>
                 <Card.Img src={image} variant='top' />
-            </a>
+            </Link>
             <Card.Body>
-                <a href={`/product/${_id}`}>
+                <Link to={`/product/${_id}`}>
                     <Card.Title as='div'>
                         <strong style={{color: 'white'}}>{name}</strong>
                     </Card.Title>
-                </a>
+                </Link>
 
                 <Card.Text as='div'>
                     <Rating value={rating} text={`${numReviews} reviews`} />
