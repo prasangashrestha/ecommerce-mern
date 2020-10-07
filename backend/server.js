61769feeb74c2b2fd44fcaad56d4bc801045f524
Prasanga <101890972@student.swin.edu.au>
2020-10-07 23:46:09 +1100

    mongodb atlas setup and create schemas for order, product and user

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv')
 const products = require('./data/products')
+const connectDB = require('./config/db')
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -24,6 +28,7 @@ const PORT = process.env.PORT || 5000
 app.listen(
     PORT,
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+            .bold
     )
 )
